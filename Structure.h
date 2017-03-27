9fb0ab11398cd84ad708c66c332468d470a79fca
Nicolas Brunet <nbrunet@localhost.localdomain>
2017-03-27 13:50:35 +0200

    ajout de affiche dans expression, structure et general

--- Structure.h
+++ Structure.h
@@ -1,25 +1,40 @@
 #ifndef _STRUCTURE_H
 #define _STRUCTURE_H
 #include "General.h"
-
+#include <iostream>
 
 class StructureControle : public InstructionV {
 	public:
 		StructureControle(InstructionV * instrv): instrv(instrv){}
 		virtual ~StructureControle() {}	
 		InstructionV* instrv;
+		void Afficher ();
 };
 class BlocIf : public StructureControle {
 	public:
 		BlocIf(Expression * expr, InstructionV * instrv, InstructionV * blocElse = NULL):StructureControle(instrv), exprCondition(expr), blocElse(blocElse){}
 		Expression* exprCondition;
 		InstructionV * blocElse;
+		void Afficher () {
+			cout<<"BLOC_IF ( "; exprCondition->Afficher(); cout<<" ) {"<<endl;
+			instrv->Afficher();
+			cout<<"}"<<endl;	
+			if(blocElse!=NULL)
+			{
+				blocElse->Afficher();
+			}
+		}
 };
 
 class BlocWhile : public StructureControle {
 	public:
 		BlocWhile(Expression * expr, InstructionV * instrv):StructureControle(instrv), exprCondition(expr) {}
 		Expression* exprCondition;
+		void Afficher () {
+			cout<<"BLOC_WHILE ( "; exprCondition->Afficher(); cout<<" ) {"<<endl;
+			instrv->Afficher();
+			cout<<"}"<<endl;	
+		}
 };
 
 class BlocFor : public StructureControle {
@@ -28,6 +43,11 @@ class BlocFor : public StructureControle {
 		Expression* exprInit;
 		Expression* exprCondition;
 		Expression* exprIncrementation;
+		void Afficher () {
+			cout<<"BLOC_FOR ( "; exprInit->Afficher(); cout<<", "; exprCondition->Afficher(); cout<<", "; exprIncrementation->Afficher(); cout<<") {"<<endl;
+			instrv->Afficher();
+			cout<<"}"<<endl;	
+		}
 };
 
 
