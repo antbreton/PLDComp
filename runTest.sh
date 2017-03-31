52b51a1fb1463ef4c87aa0022051afc1aefb11c9
antbreton <antoine.breton@insa-lyon.fr>
2017-03-31 17:16:45 +0200

    rename test files

--- runTest.sh
+++ runTest.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 nSuccesfulTests=0
-nombreTest=2
+nombreTest=3
 
 # Boucle de test #
 for ((i=1; i<=$nombreTest; i++)) # On parcours les différents Tests
  do
- 	description=`cat test/Test$i/test$i.desc`
+ 	description=`cat test/Test$i/desc`
  	
 	echo -e "\tTest$i : $description\n"
 
-	./gc+ <test/Test$i/test$i.test > test/Test$i/test$i.out 2> /dev/null
-	sed -i '/^$/d' test/Test$i/test$i.out
+	./gc+ <test/Test$i/test > test/Test$i/out 2> /dev/null
+	sed -i '/^$/d' test/Test$i/out
 	
-	diff -wB test/Test$i/test$i.out test/Test$i/test$i.expected >> /dev/null
+	diff -wB test/Test$i/out test/Test$i/expected >> /dev/null
 	stdoutValide=$?;
 
 	if [[ $stdoutValide -eq 0 ]] #Si la sortie est correcte
