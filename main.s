62d11b9a62710a248efc7c70e8f7db10a40517fb
Quentin Toulou <toulou.quentin@gmail.com>
2017-04-04 16:06:11 +0200

    pas de segfault

--- main.s
+++ main.s
@@ -1,4 +1,3 @@
-
 .text        
 .global main 
 
@@ -6,11 +5,17 @@ main_bb:
 
     pushq   %rbp 
     movq    %rsp, %rbp 
+    subq    $16, %rsp 
+movq $0, -24834(%rbp)
+movq $0, -24834(%rbp)
+    movq   $105, 0(%rbp)
+    movq    0(%rbp), %rax
+    movq    %rax, 0(%rbp)
     movq   $107, 0(%rbp)
-    call   0(%rbp)
+    movq    0(%rbp), %rax
+    movq    %rax, 0(%rbp)
 
     leave
     ret
-
 
 
