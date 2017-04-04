db3806506f2f16620729c144fc5b8ec5e1aa657a
nathan arsac <nathan.arsac@insa-lyon.fr>
2017-04-04 10:55:56 +0200

    fix seg fault, one remaining in buildIR

--- main.s
+++ main.s
@@ -1,3 +1,24 @@
 .text        
 .global main 
 
+fonction1_bb:
+
+    pushq   %rbp 
+    movq    %rsp, %rbp 
+    subq    $0, %rsp 
+
+
+    leave
+    ret
+
+main_bb:
+
+    pushq   %rbp 
+    movq    %rsp, %rbp 
+    subq    $0, %rsp 
+
+
+    leave
+    ret
+
+
