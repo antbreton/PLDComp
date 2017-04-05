c58eed3de8e55c25cbb85cb7f621ce97cefc271b
Quentin Toulou <toulou.quentin@gmail.com>
2017-04-05 08:20:25 +0200

    merge

--- main.s
+++ main.s
@@ -5,18 +5,7 @@ main:
 
     pushq   %rbp 
     movq    %rsp, %rbp 
-    subq    $24, %rsp 
-    movq    $79, %rax
-    movq    %rax, -8(%rbp)
-    movq    -8(%rbp), %rax
-    movq    %rax, -16(%rbp)
-    movq    $75, %rax
-    movq    %rax, -24(%rbp)
-    movq    $110, %rax
-    movq    %rax, -8(%rbp)
-    call   putchar
-    call   putchar
-    call   putchar
+    subq    $16, %rsp 
 
     leave
     ret
