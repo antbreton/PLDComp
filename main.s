7653c35d06c4a499911d31abbeceaa80a7642152
nathan arsac <nathan.arsac@insa-lyon.fr>
2017-04-08 20:27:08 +0200

    add tests

--- main.s
+++ main.s
@@ -6,34 +6,31 @@ main:
 
     pushq   %rbp 
     movq    %rsp, %rbp 
-    subq    $56, %rsp 
+    subq    $48, %rsp 
 
     movq    $2, %rax
     movq    %rax, -8(%rbp)
-    movl   $2, -16(%rbp)
+    movq   $6, -16(%rbp)
     movq    -8(%rbp), %rax 
     xor    -16(%rbp), %rax
     cmpq   $0, %rax 
     sete   %al 
-    movzbl %al, %rax 
+    movzbq %al, %rax 
     movq    %rax, -24(%rbp) 
     jne blocELSE
 blocIF:
-    movl   $86, -32(%rbp)
+    movq   $86, -32(%rbp)
     mov    -32(%rbp), %edi
     call   putchar
-    movl   $10, -40(%rbp)
+    movq   $10, -40(%rbp)
     mov    -40(%rbp), %edi
     call   putchar
     jmp blocAfter
 blocELSE:
-    movl   $88, -48(%rbp)
+    movq   $88, -48(%rbp)
     mov    -48(%rbp), %edi
     call   putchar
 blocAfter:
-    movl   $90, -56(%rbp)
-    mov    -56(%rbp), %edi
-    call   putchar
 
     leave
     ret
